f7da142f06477b17ebfd073d554fc8098745a12f
NB Kelly <N.B.Kelly@protonmail.com>
2025-04-12 21:01:35 +1200

    more unit tests for sources

--- test/clj/game/cards/sources_tests.clj
+++ test/clj/game/cards/sources_tests.clj
@@ -1,6 +1,7 @@
 (ns game.cards.sources-tests
   (:require
    [clojure.test :refer :all]
+   [game.core.presence :refer [get-presence]]
    [game.core :as core]
    [game.core.card :refer :all]
    [game.core.rezzing :as rezzing]
@@ -30,6 +31,26 @@
               :server :commons
               :credits 1}))
 
-;; TODO: TELE-MAIL CLUSTER
+(deftest tele-mail-cluster-test
+  (collects? {:name "Tele-Mail Cluster"
+              :cards 1})
+  (do-game
+    (new-game {:corp {:hand ["Tele-Mail Cluster" "Shardwinner"]}})
+    (play-from-hand state :corp "Tele-Mail Cluster" :council :inner)
+    (forge state :corp (pick-card state :corp :council :inner))
+    (click-card state :corp "Shardwinner")
+    (stage-select state :corp :council :outer)
+    (is (= "Shardwinner" (:title (pick-card state :corp :council :outer))))))
 
 ;; TODO: THE DRAGONS HOARD
+(deftest the-dragons-hoard
+  (collects? {:name "The Dragon’s Hoard"
+              :credits 1})
+  (do-game
+    (new-game {:corp {:hand ["The Dragon’s Hoard" "Shardwinner"]}})
+    (play-from-hand state :corp "The Dragon’s Hoard" :council :inner)
+    (click-credit state :runner)
+    (play-from-hand state :corp "Shardwinner" :council :middle)
+    (is (changed? [(get-presence (pick-card state :corp :council :middle)) 1]
+          (forge state :corp (pick-card state :corp :council :inner)))
+        "+1 presence")))
