38b5cce97e3d1c977ed09c0a79d90a7012f3f85d
NB Kelly <N.B.Kelly@protonmail.com>
2025-05-03 13:10:32 +1200

    update landing page

--- src/cljs/nr/landing.cljs
+++ src/cljs/nr/landing.cljs
@@ -5,11 +5,22 @@
    [:h2 "Welcome!"]
    [:p "This website is for the facilitation of Hubworld: Aidalon games online. Please note that we only allow for 1v1 games at this time, and we do not provide a complete implementation of the " [:u "rules"] " of the game."]
    [:h4 "Making this website better:"]
-   [:p "Jinteki.net is the product of voluntary contributions made by many individuals. This project builds on the Jinteki.net codebase. At this current time, I'm not generally accepting contributions - I need to let this codebase mature a tiny bit first. If you have questions or suggestions though, then please by all means reach out to me and I'll see what I can do."]
+   [:p "Jinteki.net is the product of voluntary contributions made by many individuals. This project builds on the Jinteki.net codebase. At this current time, I'm not accepting major contributions, but I'm happy to look at minor patches, bugfixes, and the like - I need to let this codebase mature a tiny bit before I start implemented major patches. If you have questions or suggestions though, then please by all means reach out to me and I'll see what I can do."]
    [:h4 "Rules Information - Learn to Play:"]
    [:ul.list.compact
     [:li [:a {:href "https://earthbornegames.com/learn-to-play-hubworld-aidalon/" :target "_blank"} "Earthborne Games' Learn To Play page/Quick Start Rules"]]
     [:li [:a {:href "https://docs.google.com/document/d/1AoUI8qg91jmqKJRkwI9cRBupjhBS8Jvy6O-Pti6mcig/edit?tab=t.0" :target "_blank"} "(Preview) Rules Glossary"]]]
+   [:h4 "Deckbuilding, and up to date card information"]
+   [:ul.list.compact
+    [:li "Because Hubworld: Aidalon is still in development, I'll probably be lagging a little behind in terms of available cards or updates."]
+    [:li "If you want to deckbuild or browse cards with the most up to date card information, you can use one of the following websites:"]
+    [:li [:a {:href "https://decksmith.app/hubworldaidalon/cards" :target "_blank"} "Decksmith.app's card browser and deckbuilder"]]
+    [:li [:a {:href "https://hubworld-db.com/" :target "_blank"} "hubworld-db's card browser and deckbuilder"]]]
+   [:h4 "Things went wrong!"]
+   [:ul.list.compact
+    [:li "If your decklist is displaying as invalid, it may be because of a format update, or that some of the cards have changed names. Open the decklist and save it again and see if that fixes it."]
+    [:li "Additionally, I may not have the particular cards you are using implemented yet"]
+    [:li "There's a little help section in the game window, above the chat log"]]
    [:h4 "Quick Tips, and notes about this service"]
    [:ul.list.compact
     [:li "If you hold the 'SHIFT' key when clicking an unforged card on the field, it will immediately be forged"]
