c26bc4f897379bc8b9993a5a3db78be22dee52c3
NB Kelly <N.B.Kelly@protonmail.com>
2025-05-11 15:57:42 +1200

    unit test for staging

--- test/clj/game/core/rules_test.clj
+++ test/clj/game/core/rules_test.clj
@@ -18,3 +18,16 @@
     (is (= 0 (count (get-scored state :runner))) "Not exiled")
     (forge state :corp (pick-card state :corp :council :outer))
     (is (= 1 (count (get-scored state :runner))) "Exiled to the opponent's score area!")))
+
+(deftest stage-a-card
+  (doseq [zone [:council :archives :commons]
+          slot [:inner :middle :outer]]
+    (testing "stage your own card"
+      (do-game
+        (new-game {:corp {:hand ["Barbican Gate"]}})
+        (is (changed? [(:click (get-corp)) -1]
+              (play-from-hand state :corp "Barbican Gate" zone slot))
+            "Spent 1 click to play")
+        (is (= "Barbican Gate" (:title (pick-card state :corp zone slot)))
+            "Installed the right card")
+        (is (empty? (:hand (get-corp))) "Card gone from hand")))))
